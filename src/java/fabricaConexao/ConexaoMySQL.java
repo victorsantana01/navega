142340e38da2b31faaf4330bfd346e9c0c50f06b
Lacerda <Lacerda@DESKTOP-DRUKTDH>
2020-01-23 12:32:02 -0200

    nova conexão...

--- src/java/fabricaConexao/ConexaoMySQL.java
+++ src/java/fabricaConexao/ConexaoMySQL.java
@@ -49,7 +49,8 @@ Class.forName(driverName);
  
 // Configurando a nossa conexão com um banco de dados//
     //String serverName = "node154823-env-6118138.jelasticlw.com.br";
-            String serverName = "18.188.247.78";    //caminho do servidor do BD
+            //String serverName = "18.188.247.78";    //caminho do servidor do BD - OLD
+            String serverName = "18.190.147.254";    //caminho do servidor do BD
 
             String mydatabase = "exporta";        //nome do seu banco de dados
  
